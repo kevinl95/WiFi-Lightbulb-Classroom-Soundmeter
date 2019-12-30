6aba8f53d0e6e57e4292ca5790c089837f74e268
kevinl95 <kevin.loeffler@colorado.edu>
2019-12-29 20:42:51 -0700

    Made the threshold slider work.

--- lightbulb.js
+++ lightbulb.js
@@ -1,16 +1,24 @@
+var max = 100;
+
 $(() => {
   $('#scan').on('click', function(event) {
-    const { Discovery } = require('magic-home');
+    const {
+      Discovery
+    } = require('magic-home');
     var dropdown = $('#dropdown');
     var selection = $('#selection');
     let discovery = new Discovery();
     dropdown.empty();
     discovery.scan(500).then(devices => {
-      devices.forEach(function (item, index) {
+      devices.forEach(function(item, index) {
         dropdown.append(
-            $('<x-menuitem></x-menuitem>').val(item).html('<x-label>' + item['id'] + ': ' + item['address'] + '</x-label>')
+          $('<x-menuitem></x-menuitem>').val(item).html('<x-label>' + item['id'] + ': ' + item['address'] + '</x-label>')
         );
       });
     });
+  });
+  $('#threshold').on('click', function(event) {
+    var threshold = $('#threshold');
+    max = threshold.val();
   });
 });
