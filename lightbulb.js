81992f0dfb615ec8846b2e7f0a4da64ef874ff58
kevinl95 <kevin.loeffler@colorado.edu>
2019-12-31 11:59:42 -0700

    Initial working version.

--- lightbulb.js
+++ lightbulb.js
@@ -1,52 +1,12 @@
 var max = 100;
 var stilllisten = false;
-var level = 0;
 var rgb = [];
 
-/**
- * Converts an HSL color value to RGB. Conversion formula
- * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
- * Assumes h, s, and l are contained in the set [0, 1] and
- * returns r, g, and b in the set [0, 255].
- *
- * @param   {number}  h       The hue
- * @param   {number}  s       The saturation
- * @param   {number}  l       The lightness
- * @return  {Array}           The RGB representation
- */
-function hslToRgb(h, s, l) {
-  var r, g, b;
 
-  if (s == 0) {
-    r = g = b = l; // achromatic
-  } else {
-    var hue2rgb = function hue2rgb(p, q, t) {
-      if (t < 0) t += 1;
-      if (t > 1) t -= 1;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
-    }
-
-    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    var p = 2 * l - q;
-    r = hue2rgb(p, q, h + 1 / 3);
-    g = hue2rgb(p, q, h);
-    b = hue2rgb(p, q, h - 1 / 3);
-  }
-
-  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
-
-function numberToColorHsl(i) {
-  // as the function expects a value between 0 and 1, and red = 0° and green = 120°
-  // we convert the input to the appropriate hue value
-  var hue = i * 1.2 / 360;
-  // we convert hsl to rgb (saturation 100%, lightness 50%)
-  var rgb = hslToRgb(hue, 1, .5);
-  // we format to css value and return
-  return rgb;
+function numberToColor(i) {
+  r = 255 * i;
+  g = -255 * i + 255;
+  return [r, g, 0]
 }
 
 async function startlisten(light) {
@@ -97,12 +57,13 @@ async function handleStream(stream, light) {
       }
       var average = values / length;
       let volume = Math.round(average);
+      console.log('VOLUME');
+      console.log(volume);
       var volBar = $('#vol');
-      volBar.val(volume / max);
-      var applyThresh = (max / 100) * (volume - 100) + max;
-      console.log(applyThresh);
-      level = applyThresh;
-      rgb = numberToColorHsl(applyThresh / max);
+      var scaled = volume / max;
+      volBar.val(scaled);
+      console.log(scaled);
+      rgb = numberToColor(scaled);
       console.log(rgb);
     }
   }
@@ -114,7 +75,7 @@ function handleError(e) {
 
 function changeColor(light) {
   console.log('Running change color')
-  light.setColor(rgb[0], rgb[1], rgb[2], handleError).then(success => {
+  light.setColorWithBrightness(rgb[0], rgb[1], rgb[2], 100, handleError).then(success => {
     console.log('Success!')
   });
 }
@@ -146,11 +107,16 @@ $(() => {
     stilllisten = true;
     var rawBulb = $('#dropdown').text();
     console.log(rawBulb.split(": ")[1]);
-    let light = new Control(rawBulb.split(": ")[1], {apply_masks:true});
-    startlisten(light);
-    setInterval(function() {
-      changeColor(light);
-    }, 5000);
+    let light = new Control(rawBulb.split(": ")[1], {
+      apply_masks: true,
+      wait_for_reply: false
+    });
+    light.setPower(true).then(success => {
+      startlisten(light);
+      setInterval(function() {
+        changeColor(light);
+      }, 1000);
+    });
   });
   $('#stop').on('click', function(event) {
     stilllisten = false;
